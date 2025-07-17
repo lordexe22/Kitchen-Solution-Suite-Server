c152d7bfd95cdda8cb014c87760409a09a82b607
lordexe22 <walterezequielpuig@gmail.com>
2025-07-16 23:42:30 -0300

    Add node-gyp-build package with initial implementation and documentation
    - Introduced LICENSE, README.md, and SECURITY.md files for node-gyp-build.
    - Implemented core functionality in bin.js, index.js, and node-gyp-build.js.
    - Added build-test.js for testing purposes and optional.js for handling optional dependencies.
    - Configured package.json with necessary metadata, scripts, and dependencies.

--- src/modules/users/users.middlewares.ts
+++ src/modules/users/users.middlewares.ts
@@ -1,10 +1,15 @@
+import jwt from 'jsonwebtoken';
+import bcrypt from "bcrypt";
 import { Request, Response } from "express";
 import { User } from "./users.types";
 import { pool } from "../../db/pool";
 
+
 // #middleware registerUser - Add a new user into the database
 export const registerUser = async (req: Request, res: Response) => {
   const user: User = req.body;
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(user.password, saltRounds);
   try {
     // #step 1 - save user data into the pgSQL database
     const queryResult = await pool.query(`
@@ -15,7 +20,7 @@ export const registerUser = async (req: Request, res: Response) => {
     `, [
       user.name,
       user.email,
-      user.password,
+      hashedPassword,
       user.phone || null,
       user.companyName || null,
       user.registerDate,
@@ -23,10 +28,9 @@ export const registerUser = async (req: Request, res: Response) => {
       user.accountStatus,
     ]);
     // #end-step
-    // #step 2 - return user id and success message to the client
+    // #step 2 - return message to client
     return res.status(201).json({
       mensaje: "Usuario registrado correctamente",
-      id: queryResult.rows[0].id,
     });
     // #end-step
   } catch (error: any) {
@@ -37,12 +41,11 @@ export const registerUser = async (req: Request, res: Response) => {
   }
 };
 // #end-middleware
-// #middleware - Eval if current user exist into the database
+// #middleware loginUser - Eval if current user exist into the database
 export const loginUser = async (req: Request, res: Response) => {
   const { email, password } = req.body;
-
   try {
-    // #step 1 - Search into the database if the current user exists
+    // #step 1 - Search into the database if the current user exists by email
     const resultado = await pool.query(`
       SELECT id, name, email, password, role FROM users WHERE email = $1
     `, [email]);
@@ -52,17 +55,25 @@ export const loginUser = async (req: Request, res: Response) => {
     }
 
     const usuario = resultado.rows[0];
-
-    if (usuario.password !== password) {
+    // #end-step
+    // #step 2 - Compare by password using bcrypt
+    const passwordValid = await bcrypt.compare(password, usuario.password);
+    if (!passwordValid) {
       return res.status(401).json({ success: false, message: "Contraseña incorrecta." });
     }
-
-    console.log("✔ Usuario autenticado:", usuario);
     // #end-step
-    // #step 2 - Return existing user data
+    // #step 3 - Create jwt
+    const token = jwt.sign(
+      { id: usuario.id, email: usuario.email },
+      process.env.JWT_SECRET as string,
+      { expiresIn: "7d" }
+    );
+    // #end-step
+    // #step 4 - Return jwt and success message
     return res.status(200).json({
       success: true,
       message: "Login exitoso.",
+      token,
       user: {
         id: usuario.id,
         name: usuario.name,
@@ -71,8 +82,9 @@ export const loginUser = async (req: Request, res: Response) => {
       }
     });
     // #end-step
+
   } catch (error: any) {
-    // #step 3 - handle exceptions
+    // #step 5 - handle exceptions
     console.error("❌ Error en login:", error.message);
     return res.status(500).json({ success: false, message: "Error interno del servidor." });
     // #end-step
